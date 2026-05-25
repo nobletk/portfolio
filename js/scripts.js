53ba54e423e55afa37f023afb94f99fcbb805be5
nobletk <80173312+nobletk@users.noreply.github.com>
2026-05-26 00:06:59 +0200

    feat: update the layout and add new projects

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,39 @@
-$(".js-scroll-trigger").click(function (event) {
-  event.preventDefault();
+$(".js-scroll-trigger").click(function(event) {
+    event.preventDefault();
 
-  var targetId = $(this).attr("href");
+    var targetId = $(this).attr("href");
 
-  setTimeout(function () {
-    $(".navbar-collapse").collapse("hide");
-  }, 300);
+    setTimeout(function() {
+        $(".navbar-collapse").collapse("hide");
+    }, 300);
 
-  $("html, body").animate(
-    {
-      scrollTop: $(targetId).offset().top,
-    },
-    1000,
-    "easeInOutExpo"
-  );
+    $("html, body").animate(
+        {
+            scrollTop: $(targetId).offset().top,
+        },
+        500,
+        "easeInOutExpo"
+    );
+});
+
+$(document).ready(function() {
+    const footerPlaceholder = $("#footer-placeholder");
+    if (footerPlaceholder.length) {
+        const filepath = footerPlaceholder.data("filepath");
+
+        footerPlaceholder.load(filepath, function() {
+
+            const yearSpan = document.getElementById("current-year");
+            if (yearSpan) {
+                yearSpan.textContent = new Date().getFullYear();
+            }
+
+        });
+    }
+
+    const navbarPlaceholder = $("#navbar-placeholder");
+    if (navbarPlaceholder.length) {
+        const filepath = navbarPlaceholder.data("filepath");
+        navbarPlaceholder.load(filepath);
+    }
 });
